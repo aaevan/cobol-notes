4d0c361d23a69a134dd0910c47ff17241706a2be
Aaron Evan-Browning <aaevan@gmail.com>
2020-12-17 13:01:24 -0500

    finish up chapter 1, add README.md

---
+++ iam.cbl
@@ -0,0 +1,11 @@
+      *exercise 1, chapter 1
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+       PROGRAM-BEGIN.
+           DISPLAY "I am a COBOL programmer".
+       PROGRAM-DONE.
+           STOP RUN.
