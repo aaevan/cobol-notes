7c3307e10a812ab2ace74b6260e4080750ee6da2
Aaron Evan-Browning <aaevan@gmail.com>
2020-12-27 23:32:14 -0500

    start chapter 12

---
+++ slvnd01.cbl
@@ -0,0 +1,9 @@
+      *------------------------------------------
+      * SLVND01.CBL
+      *------------------------------------------
+           SELECT VENDOR-FILE
+               ASSIGN TO "vendor"
+               ORGANIZATION IS INDEXED
+               RECORD KEY IS VENDOR-NUMBER
+               ACCESS MODE IS DYNAMIC.
+
